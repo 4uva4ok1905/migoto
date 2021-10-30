194525628001141691819526d248e6389294771e
4uva4ok <roma.fe1905@gmail.com>
2021-10-30 22:25:06 +0300

    START PRICES

--- src/js/app.js
+++ src/js/app.js
@@ -10,18 +10,45 @@ $(".js-openMenu").on("click", function () {
 const $header = $(".js-header");
 
 $(window).scroll(function () {
-
     const headerHeight = $header.height();
+    const scrollTop = $(this).scrollTop();
 
-    if ($(this).scrollTop() > headerHeight) {
+    if (scrollTop > headerHeight) {
         $header.addClass("is--hide")
     } else {
         $header.removeClass("is--hide")
     }
 
-    if ($(this).scrollTop() > 600) {
+    if (scrollTop > 600) {
         $header.addClass("is--fixed")
     } else {
         $header.removeClass("is--fixed")
     }
+
+    $(".js-counter:not(.is--finish)").each(function () {
+        if (scrollTop > $(this).offset().top - window.innerHeight) {
+            $(this).addClass("is--finish");
+            startCounter($(this))
+        }
+    });
 });
+
+
+function startCounter($el) {
+    const total = $el.data("count");
+
+    let countInterval = null;
+    let countInit = 0;
+
+    countInterval = setInterval(function () {
+        if (countInit >= parseInt(total)) {
+            clearInterval(countInterval)
+
+            $el.text(total)
+        } else {
+            countInit += 51;
+
+            $el.text(countInit)
+        }
+    }, 25);
+}
